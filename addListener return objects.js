2421483893cabcbfe7ffab840ae34f4f01fe265e
Aliya Yasir <aliya@yasglobal.com>
2016-08-24 14:08:59 +0500

    addListener returned objects information

---
+++ addListener return objects.js
@@ -0,0 +1,37 @@
+statusEvent --
+[Objectcategory: "PNConnectedCategory"
+operation: "PNSubscribeOperation"
+]
+
+statusEvent[
+category""PNUnknownCategory"
+error:true
+errorData :[
+Error: Not Found at f.<anonymous> (https://cdn.pubnub.com/sdk/javascript/pubnub.4.0.5.min.js:1:11824) at f.r.emit (https://cdn.pubnub.com/sdk/javascript/pubnub.4.0.5.min.js:1:18543) at XMLHttpRequest.n.onreadystatechange (https://cdn.pubnub.com/sdk/javascript/pubnub.4.0.5.min.js:1:15863)
+message:"Not Found"
+original:null
+response:[....]
+statusCode:404
+]
+operation:"PNUnsubscribeOperation"
+statusCode:404
+
+]
+
+
+message[
+actualChannel:null
+message:[text:"hey"]
+subscribedChannel:"my_channel"
+timetoken:"14720276573113294"
+]
+presenceEvent[
+action:"join"
+actualChannel:null
+occupancy:1
+state:undefined
+subscribedChannel:"my_channel2-pnpres"
+timestamp:1472028197
+timetoken:"14720281973160413"
+uuid:"680f51a4-950c-49f2-9c7c-3572f4fa4d13"
+]
